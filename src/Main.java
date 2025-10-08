ee306c433e95d1354049b072ee165fe37dcd8600
Schipper <havkinscy@mail.ru>
2025-10-08 04:02:29 +0300

    start change

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            Player one = new Player(25, "Oleg" );
+            Player two = new Player(17, "Slava" );
+        System.out.println(one.play());
+        System.out.println(two.play());
+    }
+    public static class Player {
+        int age;
+        String name;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public Player(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+        public int play() {
+            int total;
+            total = age + age;
+            return total;
         }
     }
 }
